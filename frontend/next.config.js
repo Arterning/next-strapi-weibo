1ab54c926d6781150229a354b803a5b17c08feb8
Arterning <ning.huang.ridson@gmail.com>
2023-09-25 10:11:35 +0800

    Revert "strapi sucks" 恢复原本端口 修改端口strapi启动报错
    This reverts commit a98fc4c1f85e290d6d19bffc6c33a71efaa723d3.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,11 +6,11 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*",
+        destination: "http://localhost:1337/api/:path*",
       },
       {
         source: "/uploads/:path*",
-        destination: "http://localhost:5000/uploads/:path*",
+        destination: "http://localhost:1337/uploads/:path*",
       },
     ];
   },
